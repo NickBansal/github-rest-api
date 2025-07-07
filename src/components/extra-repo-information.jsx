e203ca42d3dcae6119ed8de8b2f3996886816b1f
Nick Bansal <nick.bansal@autotrader.co.uk>
2025-07-07 10:14:30 +0100

    added in a more information button to set up the modal

--- src/components/extra-repo-information.jsx
+++ src/components/extra-repo-information.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { FaCodeBranch, FaExclamationCircle } from 'react-icons/fa'
-import { FaHeart, FaStar } from 'react-icons/fa6'
+import { FaArrowRight, FaHeart, FaStar } from 'react-icons/fa6'
 
 export const ExtraRepoInformation = ({ repo }) => {
+    const [infoModal, setInfoModal] = useState(false)
+
     return (
         <div className="flex flex-col border-t border-gray-300 pt-4">
             <p className="flex items-center gap-2">
@@ -20,6 +23,20 @@ export const ExtraRepoInformation = ({ repo }) => {
                 <FaExclamationCircle color="red" />
                 Issues: {repo.open_issues_count}
             </p>
+            <button
+                className="text-teal-600 hover:underline text-left cursor-pointer mt-4 flex items-center gap-2"
+                onClick={() => setInfoModal((prevState) => !prevState)}
+            >
+                More information available
+                <FaArrowRight size={20} />
+            </button>
+            {infoModal ? (
+                // <MoreInformationModal
+                //     repo={repo}
+                //     onClose={() => setInfoModal(false)}
+                // />
+                <p>HELLO</p>
+            ) : null}
         </div>
     )
 }
